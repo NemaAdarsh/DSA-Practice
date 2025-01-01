87f422b29298f172018099399f6f3033c9ecad37
Adaddy1 <adarshnema6@gmail.com>
2025-01-01 10:42:16 +0530

    169

---
+++ 150s/169.py
@@ -0,0 +1,62 @@
+'''
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+ 
+
+Follow-up: Could you solve the problem in linear time and in O(1) space?
+'''
+
+
+'''
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        Integer candidate = null;
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+        return candidate;
+        
+    }
+}
+
+'''
+
+
+from typing import List
+def majorityElement(nums: List[int]) -> int:
+    count = 0
+    candidate = None
+    for num in nums:
+        if count == 0:
+            candidate = num
+        count += (1 if num == candidate else -1)
+    return candidate
+nums = [2,2,1,1,1,2,2]
+print(majorityElement(nums)) # 2
+nums = [3,2,3]
+print(majorityElement(nums)) # 3
+nums = [1]
+# Time: O(n), Space: O(1)
