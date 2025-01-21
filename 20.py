47e56c79025ecaf80b03e8c43cdc095b370fd56f
Adaddy1 <adarshnema6@gmail.com>
2025-01-21 22:05:12 +0530

    wrong answer

--- 20.py
+++ 20.py
@@ -40,39 +40,4 @@ Constraints:
 s consists of parentheses only '()[]{}'.
 '''
 
-'''
-class Solution:
-    def isValid(self, s: str) -> bool:
-        stack = []
-        for i in s:
-            if i == '(' or i == '[' or i == '{':
-                stack.append(i)
-            elif i == ')' and len(stack) > 0 and stack[-1] == '(':
-                stack.pop()
-            elif i == ']' and len(stack) > 0 and stack[-1] == '[':
-                stack.pop()
-            elif i == '}' and len(stack) > 0 and stack[-1] == '{':
-                stack.pop()
-            else:
-                return False
-        return len(stack) == 0
-'''
 
-
-
-def isValid(s):
-    stack = []
-    for i in s:
-        if i == '(' or i == '[' or i == '{':
-            stack.append(i)
-        elif i == ')' and len(stack) > 0 and stack[-1] == '(':
-            stack.pop()
-        elif i == ']' and len(stack) > 0 and stack[-1] == '[':
-            stack.pop()
-        elif i == '}' and len(stack) > 0 and stack[-1] == '{':
-            stack.pop()
-        else:
-            return False
-    return len(stack) == 0
-# Time complexity: O(n)
-# Space complexity: O(n)
