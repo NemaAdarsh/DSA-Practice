33d29f85014be7a049b1f154d1a7c93ba5068d99
Adarsh Nema <88324861+NemaAdarsh@users.noreply.github.com>
2025-05-27 17:55:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.73%) - LeetSync

---
+++ 3321-type-of-triangle/type-of-triangle.py
@@ -0,0 +1,8 @@
+class Solution:
+    def triangleType(self, nums: List[int]) -> str:
+        a, b, c = sorted(nums)
+        if a + b <= c: return "none"
+        if a == b == c: return "equilateral"
+        if a == b or b == c: return "isosceles"
+        return "scalene"
+        
