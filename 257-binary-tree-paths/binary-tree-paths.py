79990a905206a1d2ef6bed29cfd6fac771054067
Adarsh Nema <88324861+NemaAdarsh@users.noreply.github.com>
2025-05-31 21:16:16 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (92.72%) - LeetSync

---
+++ 257-binary-tree-paths/binary-tree-paths.py
@@ -0,0 +1,5 @@
+class Solution:
+    def binaryTreePaths(self, root):
+        if not root: return []
+        if not root.left and not root.right: return [str(root.val)]
+        return [str(root.val) + '->' + path for child in (root.left, root.right) if child for path in self.binaryTreePaths(child)]
